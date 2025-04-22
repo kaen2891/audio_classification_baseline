d1a750cfd9960d0194e822db92ec69e1a484ebb2
June-Woo Kim <46586785+kaen2891@users.noreply.github.com>
2025-04-22 15:07:05 +0900

    Create get_duration2.py

---
+++ get_duration2.py
@@ -0,0 +1,22 @@
+from glob import glob
+import torchaudio
+
+
+#all_data = sorted(glob('./data/TESS_TDMS_to_CSV_output_MEMS_FINAL_WAV/*/*.wav'))
+all_data = sorted(glob('E:\wonkwang\Data\가을문단all/*.wav'))
+
+all_len = []
+for i, data in enumerate(all_data):
+    y, sr = torchaudio.load(data)
+    #print(sr)
+    
+    wav_len = y.size(-1)/sr
+    #print(wav_len)
+    
+    all_len.append(wav_len)
+    
+import numpy as np
+print(np.mean(all_len))
+print(np.max(all_len))
+print(np.min(all_len))
+    
